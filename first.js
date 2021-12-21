25aeca774644e651d5ab294ab570f30d687c950c
Ngaicode <Mjombakevin376@gmail.com>
2021-12-21 19:28:04 +0300

    use nano text editor

--- first.js
+++ first.js
@@ -1,2 +1,4 @@
-alert("i am human .after all i'm not your expectations")
-let wagwan = "that's alright with me"
+alert("i am human .after all i'm not your expectations");
+
+let wagwan = "that's alright with me";
+console.log(wagwan);
