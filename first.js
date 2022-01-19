e19ab8b3c236847cc7c2e0498be03f8bdfdb8b7c
Ngaicode <Mjombakevin376@gmail.com>
2022-01-19 23:02:28 +0300

    setting up prettier vs code extension

--- first.js
+++ first.js
@@ -1,8 +1,14 @@
-alert("i am human .after all i'm not your expectations");
+"use strict";
 
+//
 let wagwan = "that's alright with me";
 console.log(wagwan);
 
-
-
-
+let x = 23;
+if (x === 23) {
+  console.log(`yes`);
+}
+const calcAge = (birthyear) => 2022 - birthyear;
+const myAge = calcAge(2000);
+console.log(myAge);
+console.log("meow");
